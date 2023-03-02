cfb72296e605bcf4e4004ef45362eecdd2af6c8f
kranthi <lci2021014@iiitl.ac.in>
2023-03-02 23:01:47 +0530

    refactor commit

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.use(cors({ origin: true, credentials: true }));
 // app.use(express.json());
 app.use("/api/user", userRoute);
 
-app.listen(8000,() => {
+app.listen(process.env.PORT||8000,() => {
     console.log("Server started on port 8000");
 });
