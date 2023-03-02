097cb5e853464fceb0aa1f0cb0b2932598d5bd45
kranthi <lci2021014@iiitl.ac.in>
2023-03-02 19:49:30 +0530

    02-03-2023

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express();
 const userRoute = require("./routes/users")
-
+var cors = require('cors');
 
 
 const uri = "mongodb+srv://panter_quant_dev_client:Sx78JD6MtlThDsRe@cluster0.bmekpvd.mongodb.net/?retryWrites=true&w=majority"
@@ -19,6 +19,8 @@ catch(error)
 }
 
 connect();
+// dotenv.config();
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use("/api/user", userRoute);
 
