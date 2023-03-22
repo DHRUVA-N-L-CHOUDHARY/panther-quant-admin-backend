d5b3a2ca9fa09a5b13b7420b819a284c0e7d368c
kranthi <lci2021014@iiitl.ac.in>
2023-03-22 20:31:09 +0530

    1st commit

--- routes/strategies.js
+++ routes/strategies.js
@@ -1,15 +1,29 @@
 const router = require("express").Router();
 
+const { default: mongoose } = require("mongoose");
 const strategies = require("../models/strategies");
 
-router.get("/", async(req,res) =>{
-    try{
-        const stratergy = await strategies.find();
-        res.status(200).json(stratergy); 
-    }
-    catch(err)
-    {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const stratergy = await strategies.find();
+    res.status(200).json(stratergy);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/getBacktestData", async (req, res) => {
+  try {
+    const { user, sno } = req.body;
+    const data = new strategies.find({
+      $and: [
+        { user: new mongoose.Types.ObjectId(user) },
+        { stratergy_no: sno },
+      ],
+    });
+    return res.json(data);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const userRoute = require("./routes/users")
-const strategiesRoute = require("./routes/strategies")
-var cors = require('cors');
+const userRoute = require("./routes/users");
+const strategiesRoute = require("./routes/strategies");
+const indicatorRoute = require("./routes/indactors");
+var cors = require("cors");
 
+const uri =
+  "mongodb+srv://panter_quant_dev_client:Sx78JD6MtlThDsRe@cluster0.bmekpvd.mongodb.net/?retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://panter_quant_dev_client:Sx78JD6MtlThDsRe@cluster0.bmekpvd.mongodb.net/?retryWrites=true&w=majority"
-
-async function connect(){
-try {
-    await mongoose.connect(uri)
+async function connect() {
+  try {
+    await mongoose.connect(uri);
     console.log("Connected to MongoDB");
-}
-catch(error)
-{
+  } catch (error) {
     console.error(error);
-}
+  }
 }
 
 connect();
 // dotenv.config();
 app.use(cors({ origin: true, credentials: true }));
-// app.use(express.json());
+app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/strategies", strategiesRoute);
-app.listen(process.env.PORT||8000,() => {
-    console.log("Server started on port 8000");
+app.use("/api/indicators", indicatorRoute);
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
