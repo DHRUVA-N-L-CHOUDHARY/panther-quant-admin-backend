ff257f98feb8e3eb2eb6907bc5dd4685495874fd
Shubham Kumar <55661658+ShubhamxKumar@users.noreply.github.com>
2023-03-29 21:30:11 +0530

    ssl updates

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const userRoute = require("./routes/users");
 const strategiesRoute = require("./routes/strategies");
 const indicatorRoute = require("./routes/indactors");
 var cors = require("cors");
+const helmet = require("helmet");
+const fs = require("fs");
+
+app.use(helmet());
+
+const options = {
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/admin.pantherquant.io/privkey.pem"
+  ),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/admin.pantherquant.io/fullchain.pem"
+  ),
+};
 
 const uri =
   "mongodb+srv://panter_quant_dev_client:Sx78JD6MtlThDsRe@cluster0.bmekpvd.mongodb.net/?retryWrites=true&w=majority";
@@ -25,6 +38,13 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/strategies", strategiesRoute);
 app.use("/api/indicators", indicatorRoute);
-app.listen(8000, () => {
-  console.log("Server started on port 8000");
+app.listen(80, () => {
+  console.log("Server started on port 80");
 });
+
+https
+  .createServer(options, app)
+  .on("secureConnection", (socket) => {
+    socket.setTimeout(10 * 60 * 1000);
+  })
+  .listen(443);
